90ff6ada779681ac88cd00818052e9717f018b72
aidensymes <hello@aidensymes.com>
2023-08-18 12:15:37 -0700

    header animations

--- assets/js/home.js
+++ assets/js/home.js
@@ -1,8 +1,8 @@
 // On window load
 //////////////////////////////////////////////////////////////////////////
-window.onload = () => {
-    initMenu()
-  }
+document.addEventListener("DOMContentLoaded", () => {
+  initMenu();
+});
 
 // Menu
 //////////////////////////////////////////////////////////////////////////
@@ -11,44 +11,44 @@ var menuSelect;
 var menu;
 
 function initMenu() {
-    items = document.getElementsByClassName('menu__item')
-    menuSelect = document.getElementsByClassName('menus__select__item')
-    menu = document.getElementById('menu')
+  items = document.getElementsByClassName("menu__item");
+  menuSelect = document.getElementsByClassName("menus__select__item");
+  menu = document.getElementById("menu");
 
-    if (menuSelect[0]) {
-        toggleMenu(menuSelect[0])
-    }
+  if (menuSelect[0]) {
+    toggleMenu(menuSelect[0]);
+  }
 }
 
 function toggleMenu(element) {
-    const originalHeight = menu.offsetHeight;
-    menu.style.height = `${originalHeight}px`;
+  const originalHeight = menu.offsetHeight;
+  menu.style.height = `${originalHeight}px`;
 
-    const menuId = element.dataset.menu;
-    for (const item of items) {
-        if (item.dataset.menu === menuId) {
-            item.classList.add('show')
-            item.classList.remove('hide')
-        } else {
-            item.classList.remove('show')
-            item.classList.add('hide')
-        }
+  const menuId = element.dataset.menu;
+  for (const item of items) {
+    if (item.dataset.menu === menuId) {
+      item.classList.add("show");
+      item.classList.remove("hide");
+    } else {
+      item.classList.remove("show");
+      item.classList.add("hide");
     }
+  }
 
-    var newHeight = 0;
-    for (const child of menu.children) {
-        newHeight += child.offsetHeight;
+  var newHeight = 0;
+  for (const child of menu.children) {
+    newHeight += child.offsetHeight;
+  }
+
+  menu.style.height = `${newHeight}px`;
+
+  for (const option of menuSelect) {
+    if (option.dataset.menu === menuId) {
+      option.classList.add("selected");
+      option.querySelector(".check").classList.add("selected");
+    } else {
+      option.classList.remove("selected");
+      option.querySelector(".check").classList.remove("selected");
     }
-
-    menu.style.height = `${newHeight}px`;
-
-    for (const option of menuSelect) {
-        if (option.dataset.menu === menuId) {
-            option.classList.add('selected')
-            option.querySelector('.check').classList.add('selected')
-        } else {
-            option.classList.remove('selected')
-            option.querySelector('.check').classList.remove('selected')
-        }
-    }
+  }
 }
--- assets/js/main.js
+++ assets/js/main.js
@@ -1,11 +1,13 @@
-// On window load
-//////////////////////////////////////////////////////////////////////////
-window.onload = () => {
-  initButtons()
-}
+// On content loaded
+////////////////////////////////////////////////////////////////////////////////
+document.addEventListener("DOMContentLoaded", () => {
+  initButtons();
+  initScramble();
+  initWrite();
+});
 
 // Button Strike through effect
-//////////////////////////////////////////////////////////////////////////
+////////////////////////////////////////////////////////////////////////////////
 function initButtons() {
   const buttons = document.getElementsByClassName("button");
 
@@ -33,12 +35,150 @@ function initButtons() {
       } else {
         strikeURL = strikesDark.slice();
       }
-      const strike =
-        button.querySelector(".button__strike");
+      const strike = button.querySelector(".button__strike");
       if (strike) {
         strike.src = strikeURL[getRandomInt(strikeURL.length)];
-        strike.style.transform = `translate(-50%, -50%) rotate(${Math.random() * 20 + 10}deg)`
+        strike.style.transform = `translate(-50%, -50%) rotate(${
+          Math.random() * 20 + 10
+        }deg)`;
       }
     });
+  }
+}
+
+// Text animations
+////////////////////////////////////////////////////////////////////////////////
+function splitWordIntoLetters(word) {
+  word.innerHTML = word.innerText.replace(/./g, "<span>$&</span>");
+}
+
+function splitLineIntoWords(line) {
+  line.innerHTML = line.innerText.replace(/\w+/g, "<span>$& </span>");
+}
+
+function splitTextIntoLines(text) {
+  text.innerHTML = text.innerText.replace(/.+$/gm, "<div>$& </div>");
+}
+
+function getRandom(min, max) {
+  return Math.random() * (max - min) + min;
+}
+
+function randomLetterLocation(letter) {
+  letter.style.transform = `translateX(${getRandom(
+    50,
+    -50
+  )}px) translateY(${getRandom(0, 100)}px) rotate(${getRandom(20, -20)}deg`;
+}
+
+function scramble(line) {
+  splitLineIntoWords(line);
+  for (let i = 0; i <= line.children.length; i++) {
+    const word = line.children.item(i);
+    if (word) {
+      word.style.transform = `rotate(${getRandom(3, -3)}deg`;
+      splitWordIntoLetters(word);
+      for (let i = 0; i <= word.children.length; i++) {
+        const letter = word.children.item(i);
+        if (letter) {
+          letter.style.transitionTimingFunction = `cubic-bezier(${getRandom(
+            1,
+            0.8
+          )},${getRandom(0.2, 0)},${getRandom(0.2, 0)},${getRandom(1, 0.8)})`;
+
+          letter.style.transform = `translateX(${getRandom(
+            10,
+            -10
+          )}px) translateY(${getRandom(50, 300)}px) rotate(${getRandom(
+            40,
+            -40
+          )}deg`;
+
+          setTimeout(() => {
+            letter.style.transform = `rotate(${getRandom(4, -4)}deg`;
+          }, 10);
+        }
+      }
+    }
+  }
+}
+
+function write(text) {
+  splitTextIntoLines(text);
+  for (let i = 0; i <= text.children.length; i++) {
+    const line = text.children.item(i);
+    if (line) {
+      splitWordIntoLetters(line);
+      for (let i = 0; i <= line.children.length; i++) {
+        const letter = line.children.item(i);
+        if (letter) {
+          letter.classList.add("hidden");
+          setTimeout(() => {
+            letter.classList.remove("hidden");
+          }, i * 40 + 200);
+        }
+      }
+    }
+  }
+}
+
+function initScramble() {
+  const lines = document.getElementsByClassName("scramble");
+  for (const line of lines) {
+    scramble(line);
+  }
+}
+
+function initWrite() {
+  const texts = document.getElementsByClassName("write");
+  for (const text of texts) {
+    write(text);
+  }
+}
+
+// Scroll
+////////////////////////////////////////////////////////////////////////////////
+var lastScrollTop = 0;
+const triggers = document.getElementsByClassName("animation-trigger");
+
+window.addEventListener("scroll", (event) => {
+  var scrollTop = this.scrollTop;
+
+  // toggleHeader(scrollTop);
+
+  for (const trigger of triggers) {
+    animateKeyframes(trigger);
+  }
+
+  lastScrollTop = scrollTop;
+});
+
+// Header nav
+////////////////////////////////////////////////////////////////////////////////
+// function toggleHeader(scrollTop) {
+//   if ($(".fullscreen-nav").is(":visible")) {
+//     $("header").addClass("down");
+//   } else if (scrollTop == 0) {
+//     $("header").removeClass("down");
+//   } else if (scrollTop < lastScrollTop) {
+//     $("header").addClass("down");
+//   } else {
+//     $("header").removeClass("down");
+//   }
+// }
+
+// Animations
+////////////////////////////////////////////////////////////////////////////////
+function animateKeyframes(element) {
+  var elementTop = element.offsetTop,
+    elementHeight = element.offsetHeight,
+    windowTop = window.scrollTop,
+    windowHeight = window.innerHeight;
+
+  // 0% means element has come into view, 100% means element has left view.
+  var percentage =
+    (windowTop + windowHeight - elementTop) / (windowHeight + elementHeight);
+  if (percentage >= 0 && percentage <= 1) {
+    element.find(".animation-target").css("--scroll", percentage);
   }
 }
